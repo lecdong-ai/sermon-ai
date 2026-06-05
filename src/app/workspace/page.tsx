ac19789c6e24431ad2bde5870cfbf13218850210
BUNKER <bunker@BUNKERui-MacBookPro-3.local>
2026-06-05 18:52:09 +0900

    fix: update Kakao SDK integrity hash

--- src/app/workspace/page.tsx
+++ src/app/workspace/page.tsx
@@ -79,7 +79,7 @@ function WorkspacePage() {
       if (typeof window !== 'undefined' && !(window as any).Kakao) {
         const script = document.createElement('script')
         script.src = 'https://t1.kakaocdn.net/kakao_js_sdk/2.7.2/kakao.min.js'
-        script.integrity = 'sha384-TuM2S3u4vQ2sZDVFJZeHfqL6jJNE/oO3GAL7atP3o1XpqBqTxSsR/cS7JkHn7HMo'
+        script.integrity = 'sha384-TiCUE00h649CAMonG018J2ujOgDKW/kVWlChEuu4jK2vxfAAD0eZxzCKakxg55G4'
         script.crossOrigin = 'anonymous'
         script.onload = () => {
           const key = process.env.NEXT_PUBLIC_KAKAO_KEY
